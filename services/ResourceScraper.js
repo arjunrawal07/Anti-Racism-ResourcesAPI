f17cbc0245207ae8707005c1ad54a060fd601ce3
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-11-03 13:59:12 -0500

    added getWebPage function in ResourceScraper class that gets the data from the /resources endpoint and stores the data in a variable called response

--- services/ResourceScraper.js
+++ services/ResourceScraper.js
@@ -1,11 +1,20 @@
 //Logic to interpret or parse HTML from webpage
-
 const axios = require("axios");
 const cheerio = require("cheerio");
+
 class ResourceScraper {
   async getAllResources() {
     let resourcesList = [];
+
     let webpage = await this.getWebPage();
+    console.log(webpage);
     return resourcesList;
   }
+
+  async getWebPage() {
+    let response = await axios.get("https://www.google.com/alerts#3:7");
+    return response.data;
+  }
 }
+
+module.exports = ResourceScraper;

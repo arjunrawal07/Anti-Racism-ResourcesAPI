80cea184775b611ba406d1174f6ca92c55261a97
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-11-03 13:21:29 -0500

    added Read capability to get all resources

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const ResourceScraper = require("./services/ResourceScraper");
 const express = require("express");
 const app = express();
 
+//Listening for the request, then the response
+app.get("/", async (req, res) => {
+  let resourceScraper = new ResourceScraper();
+  let resourcesList = await resourceScraper.getAllResources();
+  //Returns list back to user
+  res.send(resourcesList);
+});
 app.listen(3000, function () {
   console.log("Listening on port 3000");
 });
